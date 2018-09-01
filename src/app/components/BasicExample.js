fe93f0cdf6c300833fa031468a351af5eaffba5a
Kostia Nahula <kostannagula@gmail.com>
2018-09-01 12:10:58 +0300

    Define base components

---
+++ src/app/components/BasicExample.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BaseExample = () => (
+    <div>Base example</div>
+);
+
+export default BaseExample;
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,19 @@
-console.log(123);
+import React from "react";
+import ReactDom from "react-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+
+import BaseExample from "./components/BasicExample"
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <BaseExample/>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App/>, document.getElementById("app"));
+

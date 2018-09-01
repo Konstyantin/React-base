fbfc21d7b99bb9fded609a230636bfa1251e2e97
Kostia Nahula <kostannagula@gmail.com>
2018-09-01 12:15:47 +0300

    create simple navigation with links to components

--- src/app/components/BasicExample.js
+++ src/app/components/BasicExample.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+import About from "./About";
+import Home from "./Home";
+
 
 const BaseExample = () => (
-    <div>Base example</div>
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+            <hr/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+        </div>
+    </Router>
 );
 
 export default BaseExample;
